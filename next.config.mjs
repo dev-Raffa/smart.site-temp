ad58d56b35a6728d52a227bda22b7184fb377dc0
Rafael da Conceição <rafael.conceicao@coderaw.io>
2025-07-29 13:09:46 -0300

    initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  serverExternalPackages: ['zod']
+  experimental: {
+    serverComponentsExternalPackages: ['zod'],
+  }
 };
 
 export default nextConfig;
